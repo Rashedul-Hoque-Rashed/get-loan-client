2706721b5ac11b9ced63a4fe42c7d2f3a8c1a5f6
Rashed <rashedulhoqueceo@gmail.com>
2023-12-06 23:57:38 +0600

    added footer

--- src/Shared/Footer.jsx
+++ src/Shared/Footer.jsx
@@ -1,45 +1,66 @@
+import logo from "../assets/images/logo.webp"
+import { FaFacebook, FaInstagram, FaLinkedin, FaPinterest, FaTwitter } from "react-icons/fa";
+import { BiSolidPhoneCall } from "react-icons/bi";
+import { HiOutlineMailOpen } from "react-icons/hi";
+import { IoLocationOutline } from "react-icons/io5";
+import { Link } from "react-router-dom";
+
+
 
 
 const Footer = () => {
-    return (
-        <div>
-            <footer className="footer p-10 bg-base-200 text-base-content">
-  <nav>
-    <header className="footer-title">Services</header> 
-    <a className="link link-hover">Branding</a>
-    <a className="link link-hover">Design</a>
-    <a className="link link-hover">Marketing</a>
-    <a className="link link-hover">Advertisement</a>
-  </nav> 
-  <nav>
-    <header className="footer-title">Company</header> 
-    <a className="link link-hover">About us</a>
-    <a className="link link-hover">Contact</a>
-    <a className="link link-hover">Jobs</a>
-    <a className="link link-hover">Press kit</a>
-  </nav> 
-  <nav>
-    <header className="footer-title">Legal</header> 
-    <a className="link link-hover">Terms of use</a>
-    <a className="link link-hover">Privacy policy</a>
-    <a className="link link-hover">Cookie policy</a>
-  </nav>
-</footer> 
-<footer className="footer px-10 py-4 border-t bg-base-200 text-base-content border-base-300">
-  <aside className="items-center grid-flow-col">
-    <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd" className="fill-current"><path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path></svg>
-    <p>ACME Industries Ltd. <br/>Providing reliable tech since 1992</p>
-  </aside> 
-  <nav className="md:place-self-center md:justify-self-end">
-    <div className="grid grid-flow-col gap-4">
-      <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path></svg></a>
-      <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a>
-      <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
+  return (
+    <div className="bg-[#03211b]">
+      <footer className="footer py-20 px-10">
+        <nav>
+          <img src={logo} alt="" />
+          <p className="text-white max-w-xs font-semibold my-4">Welcome to Finview, your trusted resource for financial loan reviews and comparisons. Our dedicated team of experts analyzes</p>
+          <div className="flex items-center gap-3">
+            <FaFacebook className="w-10 h-10 text-white p-2 rounded-full border border-[#074c3e] hover:bg-[#074c3e]" />
+            <FaTwitter className="w-10 h-10 text-white p-2 rounded-full border border-[#074c3e] hover:bg-[#074c3e]" />
+            <FaPinterest className="w-10 h-10 text-white p-2 rounded-full border border-[#074c3e] hover:bg-[#074c3e]" />
+            <FaLinkedin className="w-10 h-10 text-white p-2 rounded-full border border-[#074c3e] hover:bg-[#074c3e]" />
+            <FaInstagram className="w-10 h-10 text-white p-2 rounded-full border border-[#074c3e] hover:bg-[#074c3e]" />
+          </div>
+        </nav>
+        <nav>
+          <header className="text-2xl font-bold text-white mb-4">Contact</header>
+          <div className="flex items-center gap-3">
+            <BiSolidPhoneCall className="w-10 h-10 text-white p-2 rounded-full border border-[#074c3e] hover:bg-[#074c3e]" />
+            <h4 className="text-white cursor-pointer text-lg font-semibold hover:text-[#fcb650]">+1 234 567 891</h4>
+          </div>
+          <div className="flex items-center gap-3 my-2">
+            <HiOutlineMailOpen className="w-10 h-10 text-white p-2 rounded-full border border-[#074c3e] hover:bg-[#074c3e]" />
+            <h4 className="text-white cursor-pointer text-lg font-semibold hover:text-[#fcb650]">info@example.com</h4>
+          </div>
+          <div className="flex items-center gap-3">
+            <IoLocationOutline className="w-10 h-10 text-white p-2 rounded-full border border-[#074c3e] hover:bg-[#074c3e]" />
+            <h4 className="text-white cursor-pointer text-lg font-semibold hover:text-[#fcb650]">31 Brandy Way, Sutton, SM2 6SE</h4>
+          </div>
+        </nav>
+        <nav>
+          <header className="text-2xl font-bold text-white mb-4">Quick Link</header>
+          <Link to='/about' className="text-white cursor-pointer text-lg font-semibold hover:text-[#fcb650]">About us</Link>
+          <Link to='/loanReviews' className="text-white cursor-pointer text-lg font-semibold hover:text-[#fcb650]">Loan Reviews</Link>
+          <Link to='/loanComparison' className="text-white cursor-pointer text-lg font-semibold hover:text-[#fcb650]">Loan Comparison</Link>
+          <Link to='/faq' className="text-white cursor-pointer text-lg font-semibold hover:text-[#fcb650]">FAQs</Link>
+          <Link to='/blog' className="text-white cursor-pointer text-lg font-semibold hover:text-[#fcb650]">Blog</Link>
+        </nav>
+      </footer>
+      <footer className="flex px-10 py-4 border-t border-[#ffffff1e] items-center flex-col-reverse md:flex-row justify-between gap-6">
+        <aside className="items-center grid-flow-col">
+          <p className="text-white font-semibold">Copyright © {new Date().getFullYear()} <span className="text-[#fcb650]">FINVIEW</span>. Designed By  <span className="text-[#fcb650]">Rashedul Hoque</span></p>
+        </aside>
+        <nav className="md:place-self-center md:justify-self-end">
+          <div className="flex flex-wrap gap-4 justify-center">
+            <Link to='/help&support' className="text-white cursor-pointer text-sm font-semibold hover:text-[#fcb650]">Help & Support</Link>
+            <Link to='/privacyPolicy' className="text-white cursor-pointer text-sm font-semibold hover:text-[#fcb650]">Privacy policy</Link>
+            <Link to='/terms&conditions' className="text-white cursor-pointer text-sm font-semibold hover:text-[#fcb650]">Terms & Conditions</Link>
+          </div>
+        </nav>
+      </footer>
     </div>
-  </nav>
-</footer>
-        </div>
-    );
+  );
 };
 
 export default Footer;
