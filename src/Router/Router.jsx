23435235b0e61c7b404ea27c37b2ef44d698e10f
Rashed <rashedulhoqueceo@gmail.com>
2023-12-14 22:09:17 +0600

    added error page'

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -2,23 +2,31 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layouts/MainLayout";
 import Home from "../Pages/Home/Home/Home";
 import Services from "../Pages/Services/Services";
+import ServicesDetails from "../Pages/ServicesDetails/ServicesDetails";
+import Error from "../Pages/Error/Error";
 
 
 const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <MainLayout/>,
-      children: [
-        {
-            path: '/',
-            element: <Home/>
-        },
-        {
-            path: '/services',
-            element: <Services/>
+  {
+    path: "/",
+    element: <MainLayout />,
+    errorElement: <Error/>,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/services',
+        element: <Services />
+      },
+      {
+        path: '/service/details/:id',
+        element: <ServicesDetails />,
+        loader: ({params}) => fetch(`http://localhost:5000/loans/${params.id}`)
         },
       ]
     },
   ]);
 
-  export default router;
+export default router;
