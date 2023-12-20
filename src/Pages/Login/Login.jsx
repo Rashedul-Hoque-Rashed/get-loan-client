d6ef04ebaad57515d2fcfd98b62ce3ada8ec311f
Rashed <rashedulhoqueceo@gmail.com>
2023-12-20 19:31:32 +0600

    login page done

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,7 +1,16 @@
+import { LuArrowUpRight } from 'react-icons/lu';
 import banner from '../../assets/images/faq_banner.webp'
 import loginImg from '../../assets/images/sign_up.webp'
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { IoMdEye } from "react-icons/io";
+import { IoMdEyeOff } from "react-icons/io";
+
 
 const Login = () => {
+
+    const [showPass, setShowPass] = useState(false);
+
     return (
         <div>
             <div className='bg-[#03211b] overflow-x-hidden'>
@@ -17,10 +26,10 @@ const Login = () => {
             </div>
             <div className="hero px-4 md:px-10 pt-32 pb-28 max-w-[1620px] mx-auto">
                 <div className="hero-content flex-col lg:flex-row-reverse gap-20">
-                    <div className="flex-1 w-full">
+                    <div data-aos="fade-left" data-aos-duration="2000" className="flex-1">
                         <img src={loginImg} alt="" />
                     </div>
-                    <div className="card shrink-0 w-full shadow-2xl bg-base-100 flex-1">
+                    <div data-aos="fade-right" data-aos-duration="2000" className="card shrink-0 w-full shadow-2xl bg-base-100 flex-1">
                         <h4 className='px-8 text-2xl font-semibold text-[#222E48] mt-6 mb-3'>Welcome Back!</h4>
                         <h6 className='px-8 text-[#222E48] font-medium'>Sign in to your account and join us</h6>
                         <form className="card-body">
@@ -30,17 +39,25 @@ const Login = () => {
                                 </label>
                                 <input type="email" placeholder="Enter Your Email" className="input input-bordered rounded-full" required />
                             </div>
-                            <div className="form-control">
+                            <div className="form-control relative">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="password" placeholder="Enter Your Password" className="input input-bordered rounded-full" required />
-                                <label className="label">
-                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                                </label>
+                                <input type={showPass ? 'text' : 'password'} placeholder="Enter Your Password" className="input input-bordered rounded-full" required />
+                                <button onClick={() => setShowPass(!showPass)} className='absolute right-4 top-12'>
+                                {
+                                    showPass ? <IoMdEye className='text-[#074c3e] w-6 h-6'/> : <IoMdEyeOff className='text-[#074c3e] w-6 h-6'/>
+                                }
+                                </button>
                             </div>
+                            <p className='mx-1 text-[#222E48]'>Do you have an account? <Link className='text-[#074c3e] font-medium hover:underline'>
+                                SignUp
+                            </Link></p>
                             <div className="form-control mt-6">
-                                <button className="btn btn-primary">Login</button>
+                                <button className="btn text-[#074c3e] text-start border border-[#074c3e] rounded-full px-6 py-3 flex items-center gap-4 hover:text-white hover:bg-[#074c3e]">
+                                    Login
+                                    <LuArrowUpRight className='w-6 h-6' />
+                                </button>
                             </div>
                         </form>
                     </div>
